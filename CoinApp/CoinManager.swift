7cb34f54ea350c8f20c54e6229b8172f23bf022c
KenYonekura <p.p.high.llows@gmail.com>
2020-09-18 16:14:43 +0900

    first commit!完成！

--- CoinApp/CoinManager.swift
+++ CoinApp/CoinManager.swift
@@ -7,64 +7,87 @@
 //
 
 import Foundation
-
+//プロトコルは設計図の仕様を定めた仕様書
+//        ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 protocol CoinManagerDelegate {
-    func didUpdatePrice(price: String,currency: String)
+    func didUpdatePrice(price: String,btc: String,currency: String)
     func didFainalWithError(error: Error)
 }
-
+//        ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+//??????????????????????????????????????????????????????????????????????????????????????????????????
+// APIを取得するためのURLでcurrencyArrayを変更する事によって各国の通貨情報を取得する事ができる｡
 struct CoinManager {
 //    通過の単位はピッカーで変更する
-    let baseURL = "https://rest.coinapi.io/v1/exchangerate/"
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate"
     let apiKey = "B64B9BFD-3263-4DAC-8148-5B1627E5ECCB"
     let currentArray = ["USD","CNY","EUR","JPY","HKD"]
+    let btcArray = ["BTC","BCH","ETH","XRP","XEM"]
     
-//    設定したプロトコルを持ってくる｡delegateという型を指定している｡
+//    プロトコルを使用するための変数
     var delegate:CoinManagerDelegate?
-    
-    func getCoinPrice(for currency:String){
-        
-        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
-        
+//    ==================================================================================================
+//    forの中身は引数かパラメータ｡今回はStirng型でselectCurrencyから通貨の値を取得してきている
+    func getCoinPrice(currency: String,btc: String){
+//        ここで表示されるのはselectCurrencyからgetCoinPriceに引数として取得した通貨が表示される
+//        print(currency)
+//        URL生成
+        print(btc)
+        let urlString = "\(baseURL)/\(btc)/\(currency)/?apikey=\(apiKey)"
         print(urlString)
-        
-//        urlStringが正しく認識していたら､UALSessionを使えるようにしている
+//        print(urlString)
+//        ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+//        正しくURLが生成されていたら､urlStringセッションが呼び出されていた発動する｡
+//        configurationはURLSessionの設定を提供するクラス｡今回はデフォルトを使用するという意味
+//        Appleの公式ドキュメントにどう使うか記載されている
+
         if let url = URL(string: urlString) {
-//            クッキーとかキャッシュとかを(configuration: .default)←標準装備､で設定している
+//            これは､URLSessionのdataTask(標準装備)にアクセスしていろいろできる｡小さい値はここでやりとりする｡
             let session = URLSession(configuration: .default)
 //            URLSessionのdataTaskにアクセスするためのものdataTaskは標準装備
             let task = session.dataTask(with: url){
                 (data,response,error)in
+//                エラー処理
                 if error != nil{
                     self.delegate?.didFainalWithError(error: error!)
                     return
                 }
                 if let safeData = data {
+//                    parseJSONの値を取得してくる構文
                     if let bitcoinPrice = self.parseJSON(safeData) {
+//                        出力する値のフォーマットを決めている今回は小数点以下2位まで｡bitcoinPriceはパーサーJSON値から取得
                         let priceString = String(format: "%.2f", bitcoinPrice)
-                        self.delegate?.didUpdatePrice(price: priceString, currency: currency)
+//      取得した値はプロトコルで作っていたdidUpdatePriceへ返す｡priceString:仮想通貨の通貨換算の値｡btc:仮想通貨｡currency:JPYとユーロ
+                        self.delegate?.didUpdatePrice(price: priceString, btc: btc, currency: currency)
                     }
                 }
             }
+//            タスク処理を開始させる構文
             task.resume()
         }
     }
+//        ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+//        JSONの解析用に定義
     func parseJSON(_ data: Data) -> Double? {
         
 //        JSONDecoderはJSONの解析のためには必ず必要
         let decoder = JSONDecoder()
-        
+//            decode=解析をする､アクセスするのはCoinDataのselfの中身
         do {
 //            CoinDataという構造体にアクセス｡DecodeDataのrateのデータを取ってくる
             let decodeData = try decoder.decode(CoinData.self, from: data)
+//                lastPriceにrateから取ってきたデータを格納
             let lastPrice = decodeData.rate
             print(lastPrice)
             return lastPrice
         } catch {
+//                エラー処理､アクセスして値が返ってこない場合にアプリが落ちないようにしている｡エラー処理にdoCAtchを使用する
+//                ここは予めエラーを投げておく→エラーで戻ってきたらcatchが発動する仕組み
 //            アプリのエラー落ちはココで回避している
             delegate?.didFainalWithError(error: error)
             return nil
         }
     }
-
+//        ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+//     ==================================================================================================
 }
+//??????????????????????????????????????????????????????????????????????????????????????????????????
--- CoinApp/ViewController.swift
+++ CoinApp/ViewController.swift
@@ -7,82 +7,114 @@
 //
 
 import UIKit
-
+//PIckerViewを使用する時に必ず必要UIPickerViewDataSource→FIXで2つ関数を出現させる｡決まりごと｡
 class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate,CoinManagerDelegate {
-    func didUpdatePrice(price: String, currency: String) {
+    func didUpdatePrice(price: String,btc: String,currency: String) {
         
 //        同期処理/非同期処理をしている｡詳しくはググる
         DispatchQueue.main.async {
-            
+//            コインラベルをテキストとして表示する
             self.coinLabel.text = price
+//            仮想通貨の通貨単位を表示する
+            self.BtcLabel.text = btc
+//            currencyの値を表示
             self.currencyLabel.text = currency
+//
         }
         
     }
-    
+//    エラー処理として必要
     func didFainalWithError(error: Error) {
         print("error")
     }
     
+    
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
+//        Pickerの列を決める部分
 //        通貨のみなので今回は1
-        return 1
+        return 2
     }
     
     
-//    変数(coinManager)に構造体(CoinManager)←CoinManager.swiftの値を格納
+//    CoinManeger.swift内の構造体(CoinManeger)から値を取ってくるために宣言する｡何事も取得するためには空の箱を用意する
     var coinManager = CoinManager()
     
-//    ピッカービューのCellの数
+//    Pickerに表示させるために取得する構文今回は上で取得してきたcoinManegerの中のcurrencyArrayを数分だけ持ってくる｡
+//    ここでcountとは配列の要素数の事を表している｢numberOfRowsInComponent｣セルの数を決めている｡
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-//        ピッカーの中に表示するもの
-        
-        return coinManager.currentArray.count
-        
+        //        ピッカーの中に表示するもの
+//        switch文のconponentの意味が分からない｡定数式の0とか1はもともとそういう意味なのか？
+        switch component {
+        case 0:
+            return coinManager.btcArray.count
+        case 1:
+             return coinManager.currentArray.count
+        default:
+           return 0
+        }
     }
     
-//    Cellに表示するラベル
+//    PickerViewの出力をココでも司っている｡[row]とはExcelでいう行のこと｡セルに表示するラベルを確定させる｡
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         
-        return coinManager.currentArray[row]
+        switch component {
+        case 0:
+            return coinManager.btcArray[row]
+        case 1:
+            return coinManager.currentArray[row]
+        default:
+            return "error"
+        }
         
     }
     
     
     
-//    選択されたピッカーの値を取得(何をユーザーが選んでいるのかわかるところ)
+//    didSelectRowはピッカーで何が選択されたかを判別している｡ didSelectRow rowとはユーザーが出力した値はrowで出力されるよという意味
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         
-//        取得確認
-        print(row,coinManager.currentArray[row])
+//        選択されたものが何かわかるようにするプリント
+//        print(row,coinManager.currentArray[row])
+//        print(row,coinManager.btcArray[row])
+//
         
-//        入れる
+// 取ってきたい値を定数に格納する｡何か値を取得したいときは変数or定数で必ず宣言する｡今回は入れたいのはcoinManeger.currencyArray[row]
+//        pickerViewの選択が分技する場合は[pickerView.selectedRow(inComponent: 0)]とする｡switchの定数式で指定した値を使うのだろうか？
+        let selectedCurrency = coinManager.btcArray[pickerView.selectedRow(inComponent: 0)]
+        let selectedCurrency2 =
+        coinManager.currentArray[pickerView.selectedRow(inComponent: 1)]
+        print(selectedCurrency)
+        print(selectedCurrency2)
         
-        
-        let selectedCurrency = coinManager.currentArray[row]
-        
-        
-//        コインマネージャーに送る
+//        コインマネージャーの値をgetCoinPriceに送る(それはslectCurrency)の値
 //        値渡しCoinManagerに格納されている値をgetCoinPriceに送る｡それはselectedCurrencyに格納された値です｡
-        coinManager.getCoinPrice(for: selectedCurrency)
+        
+        coinManager.getCoinPrice(currency: selectedCurrency2, btc: selectedCurrency)
         
         
+//        ここでやっていることは､どの通貨が選択されているかを判別して､ユーザーが選択した通貨を元にデータを取得したいのでURLを生成しようとしている｡
         
     }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        PIckerのどのデータソースを使うのか宣言する構文｡viewDidLoad内に入れる決まりごと
         currencyPicker.dataSource = self
+//        PIckerのどのデータソースを使うのか宣言する構文｡viewDidLoad内に入れる決まりごと(delegateを使いますという宣言)
         currencyPicker.delegate = self
+        
         coinManager.delegate = self
+        
     }
 
-    
+    //StackView内のラベル左側(コインの金額が出る)
     @IBOutlet var coinLabel: UILabel!
-    
+    //StackView内のラベル右側(通貨単位が出る)
     @IBOutlet var currencyLabel: UILabel!
-    
+    //    ピッカー
     @IBOutlet var currencyPicker: UIPickerView!
+    
+    @IBOutlet var BtcLabel: UILabel!
 }
-
